36b03b8c67cb4c47d42dd73933df6ee5c6689595
hyungduk seo <hdseo82@gmail.com>
2025-12-23 04:21:09 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://search.

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'search.pstatic.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
